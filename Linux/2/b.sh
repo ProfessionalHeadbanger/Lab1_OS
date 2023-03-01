243d6a99edb054b71c59df55ea963181d169d53a
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2023-03-01 08:41:49 +0300

    Update b.sh

--- Linux/2/b.sh
+++ Linux/2/b.sh
@@ -5,6 +5,6 @@ chmod g+x /usr/bin/bezr
 sudo cp raz.lib /usr/lib
 
 sudo mkdir /share/bryantsev
-sudo mv rez.res /share/bryantsev
+sudo cp rez.res /share/bryantsev
 echo "/usr/bin/bezr" > /share/bryantsev/rez.res
 echo "/usr/lib/raz.lib" > /share/bryantsev/rez.res
